5fb784e46da15d644e890505f022007d89738c18
Brian Fletcher <brianpatrickfletcher@gmail.com>
2024-11-24 12:16:24 -0500

    Add metadata and error handling

---
+++ app/controllers/concerns/error_handler.rb
@@ -0,0 +1,40 @@
+class UnauthorizedError < StandardError; end
+
+module ErrorHandler
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActionDispatch::Http::Parameters::ParseError, with: :handle_parse_error
+    rescue_from ActionController::ParameterMissing, with: :handle_param_missing
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+    rescue_from UnauthorizedError, with: :handle_unauthorized_error
+  end
+
+  private def handle_parse_error(_exception)
+    render_error_with_metadata('Invalid JSON format', :bad_request)
+  end
+
+  private def handle_param_missing(_exception)
+    render_error_with_metadata('Query param empty or missing', :bad_request)
+  end
+
+  private def handle_record_not_found(exception)
+    render_error_with_metadata(exception.message, :not_found)
+  end
+
+  private def handle_unauthorized_error(exception)
+    render_error_with_metadata('Unauthorized request', :unauthorized)
+  end
+
+  private def handle_service_error(response)
+    error_info = JSON.parse(response[:message] || {}).with_indifferent_access
+    error_message = error_info.dig(:error, :message) || 'An unknown error occurred'
+    status = error_info.dig(:error, :code)&.to_i || 400
+
+    render_error_with_metadata(error_message, status)
+  end
+
+  private def render_error_with_metadata(message, status)
+    render json: { metadata: metadata, error: message }, status: status
+  end
+end
