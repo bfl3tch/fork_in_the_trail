d1281383dab286476a79b8e5a3766e6c54cdd160
Brian Fletcher <brianpatrickfletcher@gmail.com>
2024-11-24 16:57:11 -0500

    Add association to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+  has_many :favorites, dependent: :destroy
 
   before_validation :generate_api_key, on: :create
-
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }
   validates :api_key, presence: true, uniqueness: true
-
+  
   private def generate_api_key
     return if self.api_key.present?
 
