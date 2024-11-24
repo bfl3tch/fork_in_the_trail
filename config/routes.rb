51c2fd24b70dc56896f65dcdcb59543652a0cff6
Brian Fletcher <brianpatrickfletcher@gmail.com>
2024-11-24 14:18:19 -0500

    Add route to make it easier to generate search requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post '/search', to: 'api/v1/restaurants#search' # alias for ease of use
+
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
