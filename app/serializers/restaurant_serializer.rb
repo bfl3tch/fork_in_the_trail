51edbe694eea4d0363fd45440cd272c9a7a493af
Brian Fletcher <brianpatrickfletcher@gmail.com>
2024-11-24 15:40:56 -0500

    Add photo URL to serializer response, add tests and cassettes

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -9,6 +9,7 @@ class RestaurantSerializer
       name: @place.dig(:displayName, :text),
       address: @place[:formattedAddress],
       price_level: convert_price_level(@place[:priceLevel]),
+      photo_url: build_photo_url,
       # is_favorite: @user ? @user.favorites.exists?(name: @place.dig(:displayName, :text)) : false
     }
   end
@@ -21,5 +22,12 @@ class RestaurantSerializer
       when 'PRICE_LEVEL_VERY_EXPENSIVE' then '$$$$'
       else 'Unknown'
     end
+  end
+
+  def build_photo_url
+    photo_name = @place.dig(:photos, 0, :name)
+    return nil unless photo_name
+    
+    "https://places.googleapis.com/v1/#{photo_name}/media?key=#{ENV['GOOGLE_PLACES_API_KEY']}&maxWidthPx=400"
   end
 end
