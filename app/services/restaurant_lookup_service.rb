d112d4e259d4a622aba90866f00f37b50d5e8da6
Brian Fletcher <brianpatrickfletcher@gmail.com>
2024-11-24 14:24:22 -0500

    Add lookup service and tests

---
+++ app/services/restaurant_lookup_service.rb
@@ -0,0 +1,17 @@
+class RestaurantLookupService
+  def self.connection
+    Faraday.new(url: 'https://places.googleapis.com') do |f|
+      f.headers['Content-Type'] = 'application/json'
+      f.headers['X-Goog-Api-Key'] = ENV['GOOGLE_PLACES_API_KEY']
+      f.headers['X-Goog-FieldMask'] = 'places.displayName,places.formattedAddress,places.priceLevel'
+    end
+  end
+
+  def self.search(query)
+    response = connection.post('/v1/places:searchText') { |request|
+      request.body = { textQuery: query }.to_json
+    }
+    json_response = JSON.parse(response.body, symbolize_names: true)
+  end
+
+end
